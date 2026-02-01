af4406908bd753963cb85ec89713f06ed4c7b285
cto <connor.m.to@gmail.com>
2026-02-01 14:18:31 +0000

    feat: Initialize game state and basic card/board rendering
    - Introduce  map to define card properties.
    - Initialize  with detailed game state including player hands, decks, board, and scales.
    - Refactor  to render cards from player's hand.
    - Add  function for deck manipulation.
    - Remove  (old version) and update  structure.

--- src/inscrypion/core.cljs
+++ src/inscrypion/core.cljs
@@ -2,12 +2,47 @@
   (:require [reagent.core :as r]
             [reagent.dom.client :as rdom-client]))
 
+(def cards
+  ;; :cost [blood_cost bone_cost]
+  {:squirrel {:name "squirrel"
+             :damage 0
+             :health 1
+             :cost [0 0]  ;; :cost [blood_cost bone_cost]
+             :sigils "none"
+             :image "/img/squirrel.png"}
+            
+   :stoat {:name "stoat"
+           :damage 1
+           :health 3
+           :cost [1 0]
+           :sigils "sniper"
+           :image "/img/stoat_card.png"}} )
+
 ;; Define app state
-(defonce app-state (r/atom {:text "Welcome to Inscrypion!2"
+(defonce app-state (r/atom {:dynamic-text nil
+                            
                             :images {:cards {}
                                      :board {}}
                             
-                            :current_turn :orange 
+                            :board-piece-clicked nil
+                            :card-clicked nil
+                            
+                            :deck-shuffled? false 
+                            :card-drawn? false 
+
+                            :current-player :orange 
+
+                            :scale {:yellow 0
+                                    :orange 0}
+                            
+                            :yellow {:hand [:squirrel]
+                                     :deck [:stoat :squirrel]
+                                     :bones 0}
+                                     
+                            
+                            :orange {:hand [:squirrel :stoat :stoat :wolf ]
+                                     :deck [:stoat :squirrel :wolf ]
+                                     :bones 0}
                             
                             :board {:yellow [{:name "squirrel"
                                               :damage 0
@@ -34,65 +69,68 @@
                                               :sigils "none"
                                               :image "/img/squirrel.png"}]}
                             
-                            :scale {:yellow 0
-                                    :orange 0}
-                            
-                            :hand {:yellow [1 2 3]
-                                   :orange [{:name "stoat"
-                                             :damage 1
-                                             :health 3
-                                             :cost [0 0]
-                                             :sigils "sniper"
-                                             :image "/img/stoat_card.png"}
-                                            {:name "stoat"
-                                             :damage 1
-                                             :health 3
-                                             :cost [0 0]
-                                             :sigils "sniper"
-                                             :image "/img/stoat_card.png"}]}
+
                             } ))
 
-(def cards
-  ;; :cost [blood_cost bone_cost]
-  {:squirel {:name "squirrel"
-             :damage 0
-             :health 1
-             :cost [0 0]  ;; :cost [blood_cost bone_cost]
-             :sigils "none"
-             :image "/img/squirrel.png"}
-            
-   :stoat {:name "stoat"
-           :damage 1
-           :health 3
-           :cost [1 0]
-           :sigils "sniper"
-           :image "/img/stoat_card.png"}} )
-
-(defn create_hand []
-  (for [card  ((:current_turn @app-state) (:hand @app-state))   ]
-    [:button {:style {:background-image (str "url(" (:image card) ")")
-                      :width 123
-                      :height 195}
-              }]) )
 
 
 
+(defn shuffle-deck []
+  (prn "old: " (:orange @app-state))
+  (swap! app-state (fn [app-state]
+                     (let [cp (:current-player app-state)
+                           app-state2 (update-in app-state [cp :deck] shuffle)]
+                       (assoc-in app-state2 [:deck-shuffled?] true)
+                       )))
+  (prn "new: " (:orange @app-state))
+  
+  )
 
+(comment
+  (map-indexed list [:a :b :c])
 
+  (map-indexed (fn [idx itm]
+                 [idx itm])
+               "foobar")
+  (map (fn [a]
+         [:button a])
+       [1 2 3])
+  )
 
+(defn create-hand []
+  (let [player-data ((:current-player @app-state) @app-state) ]
+    [:div
+     (map-indexed (fn [index card-key]
+                    [:button {:style {:background-image (str "url(" (:image (cards card-key)) ")")
+                                      :width 123
+                                      :height 195}
+                              :on-click (fn [] (swap! app-state assoc-in [:l]))
+                              :key index}])
+                  (:hand player-data))]
+    
+    #_[:div
+       (for [[index card-key] index-card-pairs]
+         [:button {:style {:background-image (str "url(" (:image (cards card-key)) ")")
+                           :width 123
+                           :height 195}
+                   :on-click (fn [] (swap! app-state assoc-in [:l]))
+                   :key index}])]) )
 
-(defn create_board []
+(defn create-board []
   [:div
    (doall
     (for [segment (range 4)]
-      ^{:key segment}
       [:button {:style {:background-image (str "url("
                                                (if-let  [i (:image ((:yellow (:board @app-state)) segment))]
                                                  i
                                                  "img/board_piece_yellow.png") ")")
                         :width 123
                         :height 195}
-                :id segment}]))
+                
+                :on-click (if (or (= (:current-player @app-state) :yellow) (:board-piece-clicked? @app-state))
+                            (fn [] (swap! app-state assoc-in [:board-piece-clicked?] true)))
+                :id segment
+                :key segment}]))
    [:br]
    (doall
     (for [segment (range 4)]
@@ -104,47 +142,85 @@
                                                  "img/board_piece_orange.png") ")")
                         :width 123
                         :height 195}
-                :on-click (fn [] (print "you clicked."))
+                :on-click (if (or (= (:current-player @app-state) :yellow) (:board-piece-clicked? @app-state))
+                            (fn [] (swap! app-state assoc-in [:board-piece-clicked?] true)) ) 
                 :id (str "segment " (+ segment 4))}]))
    [:br] ] )
 
+(defn play-card []
+  (let [])
+  )
+
+(defn draw-card [card-key]
+  (prn "drawn?" (:card-drawn? @app-state))
+  (if (:card-drawn? @app-state)
+    (swap! app-state assoc :dynamic-text
+           (["You can draw a new card once per turn. You have already drawn one..."
+             "Do you understand? Only one."
+             "You have drawn a card this turn."
+             "One card per turn. Don't be greedy."] (rand-int 4) ))
+
+    (if (not= card-key nil) 
+      (let [path [(:current-player @app-state) :hand]]
+        (swap! app-state update-in path conj card-key)
+        (swap! app-state assoc-in [:card-drawn?] true) )
+      (swap! app-state assoc :dynamic-text []
+      ) )
+  ) )
+
+  #_(defn calculate-board []
+    (let [board-state (:board @app-state)]   
+
+      (for [r (range 4)]
+        )
+      )
+    )
 
 
+#_(defn end-turn []
+  (calculate-board)
 
- 
-
-
-;;(defn end_turn)
+  )
 
 
 ;;"\\wsl.localhost\Ubuntu-24.04\home\cto\workspace\inscrypion\public\img\board.png"
 
 ;; Main component
-(defn app []
+(defn app [app-state]
   [:div
-   [:p (:text @app-state)]
+   (if [(:deck-shuffled? @app-state)]
+     nil
+     (shuffle-deck))
+   
    [:br]
-   [:button {:style {:background-image "url(img/board_piece_orange.png)"
-                     :width 123
-                     :height 195}
-             }]
-   [create_board]
+   [create-board]
    [:br]
-   (create_hand)
-
+   [:h1 "hand:"]
+   (create-hand)
+   [:br]
+   [:h1 (:dynamic-text @app-state)]   ;;(prn "test")
    [:br]
    [:button {:style {:background-image "url(/img/squirrel.png)"
                      :width 123
                      :height 195}
-             :on-click (fn [] [ ])}] 
+             :on-click (fn [] (draw-card :squirrel) )}]
+   
+   [:button {:style {:background-image nil
+                     :width 123
+                     :height 195}
+             :on-click (fn []
+                         (let [current-player (:current-player @app-state)
+                               deck (:deck (current-player @app-state)) ]
+                           (draw-card (first deck)) 
+                           (swap! app-state update-in [current-player :deck] (fn [a]
+                                                                               (vec (rest a))) )) )}]
    ]
-
+  
   )     
-
   
   ;;root in an atom
-   
-
+  
+  
 (defonce root (atom nil)) 
 
  
@@ -153,8 +229,14 @@
   (let [app-element (.getElementById js/document "app")]
     (when-not @root
       (reset! root (rdom-client/create-root app-element)))
-    (rdom-client/render @root [app])))
+    (rdom-client/render @root [app app-state])))
 
 ;; Hot reload handler
 (defn ^:dev/after-load reload []
   (init)) 
+
+(comment
+  (swap! app-state update-in [:orange :hand] conj :squirrel)
+  (swap! app-state update-in [path] conj card-key)
+  (first (:deck (:orange @app-state) ) )
+  )
