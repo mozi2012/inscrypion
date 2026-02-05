7d11bd09322adffe5c6773f229a64bdc2fa58068
cto <connor.m.to@gmail.com>
2026-02-05 03:54:35 +0000

    Update core ClojureScript file.

--- src/inscrypion/core.cljs
+++ src/inscrypion/core.cljs
@@ -5,45 +5,44 @@
 (def cards
   ;; :cost [blood_cost bone_cost]
   {:squirrel {:name "squirrel"
-             :damage 0
-             :health 1
-             :cost [0 0]  ;; :cost [blood_cost bone_cost]
-             :sigils "none"
-             :image "/img/squirrel.png"}
-            
+              :damage 0
+              :health 1
+              :cost [0 0] ;; :cost [blood_cost bone_cost]
+              :sigils "none"
+              :image "/img/squirrel.png"}
+
    :stoat {:name "stoat"
            :damage 1
            :health 3
            :cost [1 0]
            :sigils "sniper"
-           :image "/img/stoat_card.png"}} )
+           :image "/img/stoat_card.png"}})
 
 ;; Define app state
 (defonce app-state (r/atom {:dynamic-text nil
-                            
+
                             :images {:cards {}
                                      :board {}}
-                            
+
                             :board-piece-clicked nil
                             :card-clicked nil
-                            
-                            :deck-shuffled? false 
-                            :card-drawn? false 
 
-                            :current-player :orange 
+                            :deck-shuffled? false
+                            :card-drawn? false
+
+                            :current-player :yellow
 
                             :scale {:yellow 0
                                     :orange 0}
-                            
+
                             :yellow {:hand [:squirrel]
-                                     :deck [:stoat :squirrel]
+                                     :deck [] ;;[:stoat :squirrel]
                                      :bones 0}
-                                     
-                            
-                            :orange {:hand [:squirrel :stoat :stoat :wolf ]
-                                     :deck [:stoat :squirrel :wolf ]
+
+                            :orange {:hand [:squirrel :stoat :stoat :wolf]
+                                     :deck [:stoat :squirrel :wolf]
                                      :bones 0}
-                            
+
                             :board {:yellow [{:name "squirrel"
                                               :damage 0
                                               :health 1
@@ -53,7 +52,7 @@
                                              nil
                                              nil
                                              nil]
-                                    
+
                                     :orange [{:name "stoat"
                                               :damage 1
                                               :health 3
@@ -67,54 +66,38 @@
                                               :health 1
                                               :cost [0 0]
                                               :sigils "none"
-                                              :image "/img/squirrel.png"}]}
-                            
+                                              :image "/img/squirrel.png"}]}}))
 
-                            } ))
+(def text-dump {:card-already-drawn
+                ["You can draw a new card once per turn. You have already drawn one..."
+                 "Do you understand? Only one."
+                 "You have drawn a card this turn."
+                 "One card per turn. Don't be greedy."]
 
+                :deck-empty
+                ["You are out of cards..."
+                 "Nothing left to draw."
+                 "Your deck is empty."
+                 "You can only draw a squirrel."
+                 "What? Trying to pluck cards from thin air?"]})
 
-
-
-(defn shuffle-deck []
-  (prn "old: " (:orange @app-state))
+(defn shuffle-decks []
   (swap! app-state (fn [app-state]
-                     (let [cp (:current-player app-state)
-                           app-state2 (update-in app-state [cp :deck] shuffle)]
-                       (assoc-in app-state2 [:deck-shuffled?] true)
-                       )))
-  (prn "new: " (:orange @app-state))
-  
-  )
-
-(comment
-  (map-indexed list [:a :b :c])
-
-  (map-indexed (fn [idx itm]
-                 [idx itm])
-               "foobar")
-  (map (fn [a]
-         [:button a])
-       [1 2 3])
-  )
+                     (let [app-state (update-in app-state [:orange :deck] shuffle)
+                           app-state (update-in app-state [:yellow :deck] shuffle)]
+                       (assoc-in app-state [:deck-shuffled?] true))))
+  nil)
 
 (defn create-hand []
-  (let [player-data ((:current-player @app-state) @app-state) ]
+  (let [player-data ((:current-player @app-state) @app-state)]
     [:div
      (map-indexed (fn [index card-key]
                     [:button {:style {:background-image (str "url(" (:image (cards card-key)) ")")
                                       :width 123
                                       :height 195}
-                              :on-click (fn [] (swap! app-state assoc-in [:l]))
+                              :on-click (fn [] (swap! app-state assoc-in [:card-clicked] index))
                               :key index}])
-                  (:hand player-data))]
-    
-    #_[:div
-       (for [[index card-key] index-card-pairs]
-         [:button {:style {:background-image (str "url(" (:image (cards card-key)) ")")
-                           :width 123
-                           :height 195}
-                   :on-click (fn [] (swap! app-state assoc-in [:l]))
-                   :key index}])]) )
+                  (:hand player-data))]))
 
 (defn create-board []
   [:div
@@ -126,11 +109,21 @@
                                                  "img/board_piece_yellow.png") ")")
                         :width 123
                         :height 195}
-                
-                :on-click (if (or (= (:current-player @app-state) :yellow) (:board-piece-clicked? @app-state))
-                            (fn [] (swap! app-state assoc-in [:board-piece-clicked?] true)))
+                :on-click (fn []
+                            (if (and (= (:current-player @app-state) :yellow)
+                                     (not= (:card-clicked @app-state)
+                                           nil))
+                              (swap! app-state assoc-in [:board-piece-clicked] segment)
+
+                              (swap! app-state (fn [app-state]
+                                                 (let [app-state
+                                                       ((swap! app-state assoc-in [:card-clicked] nil))]
+
+                                                   (swap! app-state assoc-in [:board-piece-clicked] nil)))))
+                            (prn "board-piece-clicked:" (:board-piece-clicked @app-state)))
                 :id segment
                 :key segment}]))
+
    [:br]
    (doall
     (for [segment (range 4)]
@@ -142,88 +135,89 @@
                                                  "img/board_piece_orange.png") ")")
                         :width 123
                         :height 195}
-                :on-click (if (or (= (:current-player @app-state) :yellow) (:board-piece-clicked? @app-state))
-                            (fn [] (swap! app-state assoc-in [:board-piece-clicked?] true)) ) 
+                :on-click (fn []
+                            (if  (and (= (:current-player @app-state) :orange)
+                                      (not= (:card-clicked @app-state)
+                                            nil))
+                              (swap! app-state assoc-in [:board-piece-clicked] (+ segment 4))
+
+                              (swap! app-state (fn [app-state]
+                                                 (let [app-state
+                                                       ((swap! app-state assoc-in [:card-clicked] nil))]
+
+                                                   (swap! app-state assoc-in [:board-piece-clicked] nil))))
+                              #_(do
+                                  (swap! app-state assoc-in [:card-clicked] nil)
+                                  (swap! app-state assoc-in [:board-piece-clicked] nil)))
+                            (prn "board-piece-clicked:" (:board-piece-clicked @app-state)))
                 :id (str "segment " (+ segment 4))}]))
-   [:br] ] )
+   [:br]])
 
 (defn play-card []
-  (let [])
-  )
+  (let [card-index (:card-clicked @app-state)
+        board-piece-index (:board-piece-clicked @app-state)]
+
+    (if (and (not= card-index nil)
+             (not= board-piece-index nil))
+      (do
+        (swap! app-state assoc-in [:board (:current-player @app-state) board-piece-index])))))
 
 (defn draw-card [card-key]
   (prn "drawn?" (:card-drawn? @app-state))
   (if (:card-drawn? @app-state)
-    (swap! app-state assoc :dynamic-text
-           (["You can draw a new card once per turn. You have already drawn one..."
-             "Do you understand? Only one."
-             "You have drawn a card this turn."
-             "One card per turn. Don't be greedy."] (rand-int 4) ))
+    (swap! app-state assoc-in [:dynamic-text]
+           ((text-dump :card-already-drawn) (rand-int 4)))
 
-    (if (not= card-key nil) 
+    (if (not= card-key nil)
       (let [path [(:current-player @app-state) :hand]]
         (swap! app-state update-in path conj card-key)
-        (swap! app-state assoc-in [:card-drawn?] true) )
-      (swap! app-state assoc :dynamic-text []
-      ) )
-  ) )
+        (swap! app-state assoc-in [:card-drawn?] true))
 
-  #_(defn calculate-board []
-    (let [board-state (:board @app-state)]   
+      (swap! app-state assoc-in [:dynamic-text]
+             ((text-dump :deck-empty) (rand-int 5))))))
 
-      (for [r (range 4)]
-        )
-      )
-    )
+#_(defn calculate-board []
+    (let [board-state (:board @app-state)]
 
+      (for [r (range 4)])))
 
 #_(defn end-turn []
-  (calculate-board)
-
-  )
-
+    (calculate-board))
 
 ;;"\\wsl.localhost\Ubuntu-24.04\home\cto\workspace\inscrypion\public\img\board.png"
 
 ;; Main component
 (defn app [app-state]
+  (when-not (:deck-shuffled? @app-state)
+    (shuffle-decks))
   [:div
-   (if [(:deck-shuffled? @app-state)]
-     nil
-     (shuffle-deck))
-   
    [:br]
    [create-board]
    [:br]
    [:h1 "hand:"]
    (create-hand)
    [:br]
-   [:h1 (:dynamic-text @app-state)]   ;;(prn "test")
+   [:h1 (:dynamic-text @app-state)] ;;(prn "test")
    [:br]
    [:button {:style {:background-image "url(/img/squirrel.png)"
                      :width 123
                      :height 195}
-             :on-click (fn [] (draw-card :squirrel) )}]
-   
+             :on-click (fn [] (draw-card :squirrel))}]
+
    [:button {:style {:background-image nil
                      :width 123
                      :height 195}
              :on-click (fn []
                          (let [current-player (:current-player @app-state)
-                               deck (:deck (current-player @app-state)) ]
-                           (draw-card (first deck)) 
+                               deck (:deck (current-player @app-state))]
+                           (draw-card (first deck))
                            (swap! app-state update-in [current-player :deck] (fn [a]
-                                                                               (vec (rest a))) )) )}]
-   ]
-  
-  )     
-  
-  ;;root in an atom
-  
-  
-(defonce root (atom nil)) 
+                                                                               (vec (rest a))))))}]])
 
- 
+;;root in an atom
+
+(defonce root (atom nil))
+
 ;; Initialize the app
 (defn init []
   (let [app-element (.getElementById js/document "app")]
@@ -233,10 +227,13 @@
 
 ;; Hot reload handler
 (defn ^:dev/after-load reload []
-  (init)) 
+  (init))
 
 (comment
   (swap! app-state update-in [:orange :hand] conj :squirrel)
   (swap! app-state update-in [path] conj card-key)
-  (first (:deck (:orange @app-state) ) )
+  (first (:deck (:orange @app-state)))
+
+  (def my-list (range 1 10))
+  (shuffle my-list)           ; => a shuffled version of the list, e.g., (2 5 7 0 3 9 4 8 1 6)
   )
